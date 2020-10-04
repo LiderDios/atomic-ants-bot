5cba622081238e561036f6a5e0316701c6b30a00
leitnermatias <68134378+leitnermatias@users.noreply.github.com>
2020-10-04 16:01:30 -0300

    Arreglada funcion nuevascrim
    Arreglada la forma de agregar datos al archivo .json, tambien se arreglaron los comandos $scrims y $nuevascrim

--- bot.py
+++ bot.py
@@ -1,30 +1,27 @@
 # bot.py
 import os
 import datetime
-from datetime import datetime
+#from datetime import datetime
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 import json
-
-
-# print(cadena_json)
+import asyncio
 
 def escribir(arg1, arg2):
     # Ejemplo de como guardar los datos. 
     # "13/04/20 23:00":"Alpha Dragons",
 
-    # Obtengo los parametros y los guardo en un dicc.
-    scrims = {
-        arg1:arg2
-    }
-    # Se dumpea a json
-    cadena_json = json.dumps(scrims)
+    #Abro el archivo .json y lo guardo en una variable
+    with open('datos.json', 'r') as f:
+        cadena_json = json.load(f)
+
+    cadena_json[arg1] = arg2 # Agrego los dos argumentos como nuevas entradas del diccionario .json
 
     try:
         # escribe en el archivo el dato
-        with open('datos.json', 'a') as f:
-            json.dump(cadena_json, f)
+        with open('datos.json', 'w') as f:
+            json.dump(cadena_json, f, sort_keys=True, indent=2) # Agrego el cadena_json modificado a datos.json
 
             rta={}
             rta['Estado'] = True
@@ -43,18 +40,31 @@ def escribir(arg1, arg2):
         # Retorno que se agrego bien
         return rta
 
-# # lee archivo json
-# cadena_json = eval(json.load(f))
-# with open('datos.json', 'r') as f:
-#     cadena_json = json.load(f)
+# Muestra todas las scrim programadas
+def mostrar_scrims():
+    with open('datos.json', 'r') as f:
+        cadena_json = json.load(f)
 
-#     print(cadena_json)
+    scrims = "" # Iniciar variable para ir pasando las scrims a type string.
+    for keys in cadena_json:
+        scrims += f"Fecha y hora: {keys} - Equipo: {cadena_json[keys]}\n"
+    
+    return scrims # Devuelve el string formateado, si no hay scrims programadas devuelve un string vacio.
 
-# Recibe el diccionario de scrims y devuelve un string con formato
-# Team: XXXXXX - Fecha y hora: XX/XX/XX XX:XX
-def dicc_string(diccionario):
-    pass
+# Devuelve un string que dice si hay scrim hoy.
+def scrim_hoy():
+    with open('datos.json', 'r') as f:
+        cadena_json = json.load(f)
 
+    hoy = "" # Iniciar variable para pasar la scrim de hoy.
+    for keys in cadena_json:
+        if keys[0:8] == datetime.datetime.now().strftime("%d/%m/%y"):
+            hoy += f"Fecha y hora: {keys} - Equipo: {cadena_json[keys]}\n"
+    
+    if hoy == "": # Si no se encontro ninguna scrim hoy.
+        return "Hoy no hay scrim."
+    else: # Sino devuelve la scrim programada para el dia.
+        return hoy
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -67,8 +77,13 @@ async def on_ready():
 
 @bot.command(name='scrims',help="Muestra todas las scrims.")
 async def todasscrim(ctx, arg=""):
-    await ctx.send(arg)
-    pass
+    if arg == "":
+        if mostrar_scrims() != "":
+            await ctx.send(mostrar_scrims())
+        else:
+            await ctx.send("No hay ninguna scrim programada.")
+    elif arg == "hoy":
+        await ctx.send(scrim_hoy())
 
 @bot.command(name='nuevascrim',help="Agrega una scrim nueva, los argumentos son: team, fecha y hora en formato: DD/MM/AA HH:MM")
 async def nuevascrim(ctx, arg1, arg2):
@@ -79,7 +94,7 @@ async def nuevascrim(ctx, arg1, arg2):
     # el comando funciona asi. arg1 arg2
     # $nuevascrim "09/19/18 13:55" asdf
 
-    datetime_object = datetime.strptime(arg1, '%m/%d/%y %H:%M')
+    datetime_object = datetime.datetime.strptime(arg1, '%d/%m/%y %H:%M')
 
     print(datetime_object, "formato fecha hora.")
 
