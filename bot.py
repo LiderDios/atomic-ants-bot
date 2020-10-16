0eb6ff243f3ea0db5a0547b85482b38cc8c4b49c
LiderDios <elias_rosario_19@hotmail.com>
2020-10-15 22:15:14 -0300

    agrego selenium
    agrego selenium

--- bot.py
+++ bot.py
@@ -7,9 +7,104 @@ from dotenv import load_dotenv
 from discord.ext import commands
 import json
 import asyncio
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+input_de_persona_para_enviar_msg = '//*[@id="side"]/div[1]/div/label/div/div[2]'
+
+
+driver = webdriver.Chrome(executable_path="c:/Users/lider/Documents/GitHub/atomic-ants-bot/chromedriver.exe")
+# abre la web de wsp
+driver.get("https://web.whatsapp.com/")
+# espera
+# wait = WebDriverWait(driver, 20)
+# cabecera
+# headers = {'Content-type': 'application/x-www-form-urlencoded'}
+
+def enviar_msg(name, mensaje=None):
+    '''
+    Esta funcion recibe un nombre, el cual se usara para buscar en el buscador
+    de personas de whatsapp y una vez localizado se le enviara el menu
+
+                Parámetros:
+                name (str)
+                mensaje (str)
+
+            Funciones:
+                at.utiles.hilos -> respuesta
+
+        '''
+    # Me aseguro que si me sale un error, le aviso al server
+    try:
+        try:
+            # Busco el path del buscador de personas
+            # elem = driver.find_element_by_xpath(
+            #        "//*[@id='side']/div[1]/div/label/div")
+            elem = driver.find_element_by_xpath\
+                (input_de_persona_para_enviar_msg)
+                # "//input[@title='Buscar o empezar un chat nuevo']")
+            elem.send_keys("")
+            # Hago click ahi
+            elem.click()
+            # Pongo el nombre que se recibio
+            print ("pongo el nombre q recibo",name)
+            elem.send_keys(name)
+            time.sleep(3)
+            # Espero 3 (tres) segundos
+            time.sleep(0.3)
+            # Hago clic en el nombre encontrado
+            driver.find_element_by_xpath("//*[@title='" + name + "']").click()
+            print ("No pude hacer click en la persona obtenida")
+            # Espero 3 (tres) segundos
+            time.sleep(0.3)
+        except:
+            pass
+            # print "No pude encontrar a la persona"
+
+        # Busco el campo de texto para escribir
+        elem = driver.find_element_by_xpath("//div[@data-tab='1']")
+        # Hago clic ahi
+        elem.click()
+        # Envio el mensaje con enter
+        elem.send_keys(mensaje + Keys.ENTER)
+        # Espero 3 segundos
+        time.sleep(0.3)
+    # Si no pude encontrar el buscador de personas
+    except Exception as erro:
+        # Limpio , ponele
+
+        elem = driver.find_element_by_xpath(
+                "//*[@id='side']/div[1]/div/label/div")
+        # elem = driver.find_element_by_xpath(
+        #         "//*[@id='side']/div[1]/div/label/div")
+        # Dejo el campo vacio
+        elem.send_keys("")
+        # y aviso que no pude enviar el msg
+        print ("No pude enviar el mensaje", erro)
+
+
+archivo = "datos.json"
+# verifica que exista el archivo, si no existe, lo crea.
+if not os.path.isfile(archivo):
+    try: # Si el archivo .json ya existe directamente pasar los args
+
+        #Abro el archivo .json y lo guardo en una variable
+        with open('datos.json', 'r') as f:
+            cadena_json = json.load(f)
+
+        cadena_json[""] = "" # Agrego los dos argumentos como nuevas entradas del diccionario .json
+
+    except Exception as err: # Si falla al abrir el .json porque no existe lo crea primero y despues lo pasa
+        print("Error al crear el archivo .json debido a:", err)
+        with open('datos.json', 'w') as f:
+            json.dump({}, f, sort_keys=True, indent=2)
 
 # escribir
-def escribir(arg1, arg2):
+def escribir(arg1, arg2, arg3):
     # Ejemplo de como guardar los datos. 
     # "13/04/20 23:00":"Alpha Dragons",
 
@@ -19,7 +114,7 @@ def escribir(arg1, arg2):
         with open('datos.json', 'r') as f:
             cadena_json = json.load(f)
 
-        cadena_json[arg1] = arg2 # Agrego los dos argumentos como nuevas entradas del diccionario .json
+        cadena_json[arg1] = arg2, arg3 # Agrego los dos argumentos como nuevas entradas del diccionario .json
 
     except Exception as err: # Si falla al abrir el .json porque no existe lo crea primero y despues lo pasa
         print("Creando archivo .json debido a:", err)
@@ -30,7 +125,7 @@ def escribir(arg1, arg2):
         with open('datos.json', 'r') as f:
             cadena_json = json.load(f)
 
-        cadena_json[arg1] = arg2 # Agrego los dos argumentos como nuevas entradas del diccionario .json
+        cadena_json[arg1] = arg2, arg3 # Agrego los dos argumentos como nuevas entradas del diccionario .json
 
 
     try:
@@ -92,6 +187,35 @@ async def on_ready():
 
 @bot.command(name='scrims',help="Muestra todas las scrims.")
 async def todasscrim(ctx, arg=""):
+    
+    # enviar_msg("jorge", "prueba de envio msg automatico y puto el que lee")
+
+
+
+    # message = "This Message is sent via DM"
+    # # await bot.send_message("user", message)
+    # await ctx.author.send(message)
+
+    user = ctx.message.server.get_member("LiderDios#4293")
+    await ctx.send_message(user,"hola")
+
+    # This works ^
+    archivo = "datos.json"
+    # verifica que exista el archivo, si no existe, lo crea.
+    if not os.path.isfile(archivo):
+        try: # Si el archivo .json ya existe directamente pasar los args
+
+            #Abro el archivo .json y lo guardo en una variable
+            with open('datos.json', 'r') as f:
+                cadena_json = json.load(f)
+
+            cadena_json[""] = "" # Agrego los dos argumentos como nuevas entradas del diccionario .json
+
+        except Exception as err: # Si falla al abrir el .json porque no existe lo crea primero y despues lo pasa
+            print("Error al crear el archivo .json debido a:", err)
+            with open('datos.json', 'w') as f:
+                json.dump({}, f, sort_keys=True, indent=2)
+                
     if arg == "":
         if mostrar_scrims() != "":
             await ctx.send(mostrar_scrims())
@@ -101,10 +225,10 @@ async def todasscrim(ctx, arg=""):
         await ctx.send(scrim_hoy())
 
 @bot.command(name='nuevascrim',help="Agrega una scrim nueva, los argumentos son: team, fecha y hora en formato: DD/MM/AA HH:MM")
-async def nuevascrim(ctx, arg1, arg2):
+async def nuevascrim(ctx, arg1, arg2, arg3=''):
 
     # Recupero la respuesta de la funcion escribir 
-    rta = escribir(arg1, arg2)
+    rta = escribir(arg1, arg2, arg3)
     
     # el comando funciona asi. arg1 arg2
     # $nuevascrim "09/19/18 13:55" asdf
@@ -139,6 +263,7 @@ async def limpiar_archivo(ctx):
      with open("datos.json", "w") as output: 
           print("Archivo borrado ")
     await ctx.send(f"Listo, {ctx.author}, el archivo se limpió.")
+
 
 
 
